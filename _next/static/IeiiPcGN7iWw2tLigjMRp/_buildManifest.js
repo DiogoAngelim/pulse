118f3db5f51d22a4c038ca6f4b3e6229058c405c
DiogoAngelim <DiogoAngelim@users.noreply.github.com>
2026-03-27 02:48:59 +0000

    deploy: 657b841941b49423679be50a809888c14753ccc4

--- _next/static/IeiiPcGN7iWw2tLigjMRp/_buildManifest.js
+++ _next/static/IeiiPcGN7iWw2tLigjMRp/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/pulse//_next/:path+",
+        "destination": "/pulse/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
